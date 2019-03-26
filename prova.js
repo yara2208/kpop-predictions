31e29d5516c8891cfb71bbadae7699c375b67eb7
yara2208 <48946973+yara2208@users.noreply.github.com>
2019-03-26 12:50:50 +0100

    Update prova.js

--- prova.js
+++ prova.js
@@ -10,21 +10,41 @@ var predictions = [
   "sunmi will eat a croissant",
   "a group under jyp will make cheese on a variety show and jyp himself will slap the cheese like people who sell cars",
   "someone will create new zodiac signs based on loona members",
-  "twice dahyun wil dye her hair in rainbow colors and scream 'gay rights'",
+  "twice dahyun will dye her hair in rainbow colors and scream 'gay rights'",
   "red velvet irene will defeat thanos",
   "blackpink will be legally allowed by the korean government to kill 3 people ",
   "apink will find the philosopher stone and rule the world",
   "red velvet will learn how to wield swords for their next comeback",
   "exid hani will take you on a date and give you her jacket",
   "mamamoo will come to your house and do your homework",
-  "f(x)",
-  "IU"
+  "f(x) will...have a comeback...maybe...",
+  "iu will adopt the twice members and her own daughters",
+  "next big music award on mars, idols will perform in spacesuits",
+  "gfriend will show up at mamas and sing gregorian chants",
+  "wjsn will open a youtube channel where they prank other idols",
+  "sm will debut nct antarctica, line up: 10 penguins wearing wigs",
+  "park bom will have a nice and peaceful life, not a prediction just my heartfelt wish for her",
+  "pristin will become a bowling team",
+  "gugudan",
+  "oh my girl",
+  "chungha",
+  "snsd",
+  "boa",
+  "clc",
+  "weki meki will adopt dachshund named sausage and sausage will appear in their mvs wearing cute outfits that match with the girls' outfits",
+  "g(i)-dle will cover rick astley's never gonna give you up",
+  "lovelyz",
+  "new girl group named flower girls, cute concept, main genre: death metal",
+  "tiffany young",
+  "new variety show where canadians idols live in australia for a week and australian idols live in canada for a week",
+  "ah",
+  "text"
 ];
 
 
 function randomNumber(){
   try{
-    number = Math.floor((Math.random() * 20) + 1); /*(numero a caso tra 1 e 100)*/
+    number = Math.floor(Math.random() * 40); 
     result.style.display='';
     result.textContent='Your Prediction: '+predictions[number];
     return;
